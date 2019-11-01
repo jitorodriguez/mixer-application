778818f02b4cfdd9d5a01ea906adb6d584791322
Jorge Rodriguez <jito.rodriguez@outlook.com>
2019-10-31 20:00:49 -0400

    fixes tsc errors in data.js file. Added types for request.

--- src/data.ts
+++ src/data.ts
@@ -7,9 +7,8 @@
 
 import Mixer, { IOAuthProviderOptions, IOptionalUrlRequestOptions } from "@mixer/client-node";
 
-const client = new Mixer.Client(new Mixer.DefaultRequestRunner() );
+const client: Mixer.Client = new Mixer.Client();
 const options: IOAuthProviderOptions = {clientId : "Click here to get your Client ID!"};
-const getQueryOptions: IOptionalUrlRequestOptions = {}; 
 
 // @ts-ignore
 const provider = new Mixer.Provider(client, options);
@@ -23,26 +22,24 @@ const twitchBuyovers = Array("Ninja", "Shroud");
 
 // tslint:disable-next-line:no-console
 console.log("Mixer bought streamers...");
+
 twitchBuyovers.forEach( (streamer) => {
     client.request("GET", `channels/${streamer}`)
     .then((res) => {
-        const channel = res.body;
+        const channel: (IOptionalUrlRequestOptions["body"]) = res.body;
 
         // tslint:disable-next-line:no-console
         console.log(`${channel.user.username}[${channel.online}]: Followers-Total[${channel.numFollowers}] Viewers-Total[${channel.viewersTotal}] Viewers-Current[${channel.viewersCurrent}]`);
     })
     .then(() => {
-        client.request("GET", "/channels", {
-            qs: {
-                fields: "user,online,viewersTotal,numFollowers,viewersCurrent",
-                order: "viewersCurrent:DESC",
-                limit: 10
-            },
-        }).then((res) => {
+        const obj: any = {fields: "", order: "", limit: 10};
+        client.request("GET", "/channels", obj)
+        .then((res) => {
             // tslint:disable-next-line:no-console
             console.log("Top 10 Online streamers by current viewers...");
+            const results: (IOptionalUrlRequestOptions["body"]) = res.body;
 
-            for (const channel of res.body) {
+            for (const channel of results) {
 
                 // tslint:disable-next-line:no-console
                 console.log(`${channel.user.username}[${channel.online}]: Viewers-Current[${channel.viewersCurrent}] Followers-Total[${channel.numFollowers}] Viewers-Total[${channel.viewersTotal}]`);
